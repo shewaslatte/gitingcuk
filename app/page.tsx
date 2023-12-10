2737d647c645a29fb6f5a17b621789a773003392
shewaslatte <92186264+shewaslatte@users.noreply.github.com>
2023-12-10 07:57:14 +0700

    ✨ feat(app): add ModeToggle component to Home page for theme switching 🎨 style(app): improve Home page layout with flexbox 📦 feat(components): add AsyncCmd, Cmd, Modals, ModeToggle, TopBar, and ThemeProvider components 🔧 refactor(components): use client-side rendering for new components 🔀 feat(components): integrate theme switching in ModeToggle component 🔧 refactor(components): use keyboard shortcuts for command dialog in AsyncCmd and Cmd components 🔧 refactor(components): use dropdown menu for theme selection in ModeToggle component 🔧 refactor(components): use settings icon in TopBar component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import { ModeToggle } from "@/components/commons/mode-toggle"
+
 export default function Home() {
-  return <div></div>
+  return (
+    <div className="flex h-screen w-full items-center justify-center">
+      <ModeToggle />
+      <span>v0.1.0-alpha</span>
+    </div>
+  )
 }
